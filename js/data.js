c0b7ae458810ac9179f203c259374c8e92a37daf
Shingo_ <kharkovoy95@gmail.com>
2022-07-11 15:41:17 +0300

    MscPlayer

---
+++ js/data.js
@@ -0,0 +1,38 @@
+let songs = [
+    {
+        name: 'part 1',
+        path: 'music/01 Luv(sic).mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover1.png'
+    },
+    {
+        name: 'part 2',
+        path: 'music/02 Luv(sic) Part 2.mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover2.jpg'
+    },
+    {
+        name: 'part 3',
+        path: 'music/03 Luv(sic) Part 3.mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover3.jpg'
+    },
+    {
+        name: 'part 4',
+        path: 'music/04 Luv(sic) Part 4.mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover4.jpg'
+    },
+    {
+        name: 'part 5',
+        path: 'music/05 Luv(sic) Part 5.mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover5.jpg'
+    },
+	{
+		name: 'part 6',
+        path: 'music/06 Luv(sic) Grand Finale.mp3',
+        artist: 'Nujabes',
+        cover: 'img/cover6.jpg'
+	}
+]
